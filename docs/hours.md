4e94b95485074e4708a26a0e2d70cfd8d2b8e494
Pekka <75335748+Pekk4@users.noreply.github.com>
2025-05-31 22:46:20 +0300

    Design more layout to test with Keycloak

--- docs/hours.md
+++ docs/hours.md
@@ -7,6 +7,7 @@
 | 27.3.| 6 | Designing and PoCing the calendar & date picking |
 | 28.3.| 4 | Fighting with modal from old project, building new from scratch |
 | 29.3.| 2 | Working with modals and designing calendar functionality |
-| 29.5.| 1 | Investigate and deploy Prettier in the project |
-| 30.5.| 4 | Investigate Keycloak for IAM purposes |
-| Sum | 21 | |
+| 29.5.| 1 | Investigating and deploying Prettier in the project |
+| 30.5.| 4 | Investigating Keycloak for IAM purposes |
+| 31.5.| 8 | Testing Keycloak, designing frontend |
+| Sum | 29 | |
--- frontend/src/components/Home.tsx
+++ frontend/src/components/Home.tsx
@@ -1,13 +1,28 @@
+import useKeycloak from '../hooks/useKeycloak';
+
+import BookingCalendar from './BookingCalendar';
+
 const HomeDemo = () => {
+  const { keycloak, authenticated, admin } = useKeycloak();
+
+  console.log(keycloak?.tokenParsed);
   return (
     <div className="h-screen w-screen grid grid-rows-3 justify-center items-center text-center">
       <div>
         <p className="text-xl font-bold">Book a bike!</p>
       </div>
-      <div className="">
-        <p>Kirjaudu sisään varataksesi moottoripyörän!</p>
+      {authenticated ? (
+        <div>
+          <BookingCalendar />
+        </div>
+      ) : (
+        <div className="">
+          <p>Kirjaudu sisään varataksesi moottoripyörän!</p>
+        </div>
+      )}
+      <div>
+        <p>Placeholder</p>
       </div>
-      <div></div>
     </div>
   );
 };

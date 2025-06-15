09eca8495d7071deb93b71bc0c76caffe59a5372
Pekka <75335748+Pekk4@users.noreply.github.com>
2025-06-15 18:59:38 +0300

    Refactor db methods

--- backend/pkg/db/booking.go
+++ backend/pkg/db/booking.go
@@ -1,30 +1,23 @@
 package db
 
 import (
+	"database/sql"
 	"fmt"
 
 	t "github.com/pekk4/bookabike/backend/pkg/types"
 )
 
 //
-//
 // Consider deploying GORM here
 // https://gorm.io/
 //
-//
-
-type BookingRepository interface {
-	CreateBooking(b t.Booking) (t.Booking, error)
-	GetAllBookings() ([]t.Booking, error)
-	//GetBooking(id int) (t.Booking, error)
-}
 
 func (c conn) CreateBooking(b t.Booking) (t.Booking, error) {
 	query := `
-        INSERT INTO bookings (start_date, end_date, status, user_id)
-        VALUES ($1, $2, $3, $4)
-        RETURNING id, start_date, end_date, status, created_at
-    `
+		INSERT INTO bookings (start_date, end_date, status, user_id)
+		VALUES ($1, $2, $3, $4)
+		RETURNING id, start_date, end_date, status, created_at
+	`
 	var booking t.Booking
 
 	err := c.db.QueryRow(
@@ -41,17 +34,30 @@ func (c conn) CreateBooking(b t.Booking) (t.Booking, error) {
 		&booking.CreatedAt,
 	)
 	if err != nil {
+		// TODO: error handling and logging
 		return t.Booking{}, fmt.Errorf("CreateBooking: %w", err)
 	}
-
 	return booking, nil
 }
 
-func (c conn) GetAllBookings() ([]t.Booking, error) {
-	query := `SELECT id, start_date, end_date, status, user_id, created_at FROM bookings`
-	rows, err := c.db.Query(query)
+func (c conn) GetBookings(userID string) ([]t.Booking, error) {
+	var rows *sql.Rows
+	var err error
+
+	query := `
+		SELECT id, start_date, end_date, status, user_id, created_at
+		FROM bookings
+	`
+	if userID == "" {
+		rows, err = c.db.Query(query)
+	} else {
+		query += " WHERE user_id = $1"
+		rows, err = c.db.Query(query, userID)
+	}
+
 	if err != nil {
-		return nil, fmt.Errorf("GetAllBookings: %w", err)
+		// TODO: error handling and logging
+		return nil, fmt.Errorf("GetBookings: %w", err)
 	}
 	defer rows.Close()
 
@@ -68,44 +74,25 @@ func (c conn) GetAllBookings() ([]t.Booking, error) {
 			&booking.UserID,
 			&booking.CreatedAt,
 		); err != nil {
-			return nil, fmt.Errorf("GetAllBookings: %w", err)
+			// TODO: error handling and logging
+			return nil, fmt.Errorf("GetBookings: %w", err)
 		}
 		bookings = append(bookings, booking)
 	}
 	if err := rows.Err(); err != nil {
-		return nil, fmt.Errorf("GetAllBookings: %w", err)
+		// TODO: error handling and logging
+		return nil, fmt.Errorf("GetBookings: %w", err)
 	}
 	return bookings, nil
 }
 
-func (c conn) GetBookingsByUserID(userID string) ([]t.Booking, error) {
-	//log.Println("GetBookingsByUserID called with userID:", userID)
-	query := `SELECT id, start_date, end_date, status, user_id, created_at FROM bookings WHERE user_id = $1`
-	rows, err := c.db.Query(query, userID)
+func (c conn) CountActiveBookingsForUser(userID string) (int, error) {
+	var count int
+
+	query := `SELECT COUNT(*) FROM bookings WHERE user_id = $1 AND status != 'wished'`
+	err := c.db.QueryRow(query, userID).Scan(&count)
 	if err != nil {
-		return nil, fmt.Errorf("GetBookingsByUserID: %w", err)
+		return 0, fmt.Errorf("GetUsersActiveBookingsCount: %w", err)
 	}
-	defer rows.Close()
-
-	var bookings []t.Booking
-
-	for rows.Next() {
-		var booking t.Booking
-
-		if err := rows.Scan(
-			&booking.ID,
-			&booking.StartDate,
-			&booking.EndDate,
-			&booking.Status,
-			&booking.UserID,
-			&booking.CreatedAt,
-		); err != nil {
-			return nil, fmt.Errorf("GetBookingsByUserID: %w", err)
-		}
-		bookings = append(bookings, booking)
-	}
-	if err := rows.Err(); err != nil {
-		return nil, fmt.Errorf("GetBookingsByUserID: %w", err)
-	}
-	return bookings, nil
+	return count, nil
 }

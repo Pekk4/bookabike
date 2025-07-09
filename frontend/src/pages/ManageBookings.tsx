53f81ccb2e46d85405072b60ad11d5268c8d3bc1
Pekka <75335748+Pekk4@users.noreply.github.com>
2025-07-09 13:34:50 +0300

    Add modal confirmation for booking actions

--- frontend/src/pages/ManageBookings.tsx
+++ frontend/src/pages/ManageBookings.tsx
@@ -1,12 +1,15 @@
 import { useEffect, useState } from 'react';
 import { useLocation } from 'react-router-dom';
+import CircularProgress from '@mui/material/CircularProgress';
 
 import { useAdminService } from '../services/adminService';
+import useModal from '../hooks/useModal';
 
 import { UserDataBooking, BookingStatus as b } from '../types';
 import BookingsManager from '../components/BookingsManager';
 
 const ManageBookings = () => {
+  const { showModal, hideModal } = useModal();
   const location = useLocation();
   const { getAllBookings, updateBookingStatus } = useAdminService();
   const [bookings, setBookings] = useState<UserDataBooking[]>([]);
@@ -31,15 +34,33 @@ const ManageBookings = () => {
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [location.pathname]);
 
+  //useEffect(() => {
+  //  if (bookings.length > 0) {
+  //    console.log('Bookings:', Array.from(bookings));
+  //  }
+  //}, [bookings]);
+
   const handleConfirm = (bookingId: number) => {
-    handleUpdate(bookingId, b.Confirmed);
+    showModal(
+      'Haluatko varmasti vahvistaa varauksen?',
+      'ask',
+      () => handleUpdate(bookingId, b.Confirmed),
+      () => hideModal()
+    );
   };
 
   const handleReject = (bookingId: number) => {
-    handleUpdate(bookingId, b.Canceled);
+    showModal(
+      'Haluatko varmasti hylätä varauksen?',
+      'ask',
+      () => handleUpdate(bookingId, b.Canceled),
+      () => hideModal()
+    );
   };
 
   const handleUpdate = async (bookingId: number, status: b) => {
+    showModal(<CircularProgress color="inherit" />);
+
     try {
       const { data: updatedBooking } = await updateBookingStatus(bookingId, status);
 
@@ -58,6 +79,8 @@ const ManageBookings = () => {
       // TODO: handle properly
       console.error('Error updating booking:', error);
     }
+
+    hideModal();
   };
 
   return (
